9753a9a611c2e1142d8ad1f41999be2827c1ef1c
xxsimoxx <simone@gieffeedizioni.it>
2024-07-25 14:18:43 +0200

    Finalize v1.0.0

--- cp-integrity-checker.php
+++ cp-integrity-checker.php
@@ -4,8 +4,8 @@
  * -----------------------------------------------------------------------------
  * Plugin Name: ClassicPress Integrity Checker
  * Description: Check core files against MD5.
- * Version: 0.0.1
- * Requires PHP: 7.4
+ * Version: 1.0.0
+ * Requires PHP: 5.6
  * Requires CP: 1.0
  * Author: Simone Fioravanti
  * Author URI: https://software.gieffeedizioni.it
@@ -30,6 +30,9 @@ class CPIntegrityChecker {
 	const SLUG       = 'cp-integrity-checker';
 
 	public function __construct() {
+		if (!function_exists('classicpress_version')) {
+			return;
+		}
 		add_action('admin_menu', [$this, 'create_settings_menu'], 100);
 	}
 
@@ -55,10 +58,7 @@ class CPIntegrityChecker {
 		}
 		$response = wp_remote_retrieve_body($response);
 		$data = json_decode($response, true);
-		if ($data === null) {
-			return false;
-		}
-		if (!isset($data['checksums'])) {
+		if (!is_array($data) ||	!isset($data['checksums']) || !is_array($data['checksums'])) {
 			return false;
 		}
 
@@ -77,16 +77,28 @@ class CPIntegrityChecker {
 	}
 
 	public function render_menu () {
+		/**
+		 * Filters the API URL.
+		 *
+		 * @param string $url ClassicPress checksums API URL.
+		 *                    Default https://api-v1.classicpress.net/v1/checksums/md5/
+		 */
 		$url     = apply_filters('cpic-url', 'https://api-v1.classicpress.net/v1/checksums/md5/');
+		/**
+		 * Filters the ClassicPress version number.
+		 *
+		 * @param string $version ClassicPress version.
+		 *                        Default to current version as from classicpress_version()
+		 */
 		$version = apply_filters('cpic-version', classicpress_version());
 
 		echo '<div class="wrap">';
 		echo '<div class="cpic cpic-general">';
 		echo '<h1>'.esc_html__('ClassicPress Integrity Checker', 'cp-integrity-checker').'</h1>';
-		echo '<p>'.esc_html__('Check core files against MD5.', 'cp-integrity-checker').'<p>';
-		echo '<p>'.sprintf(esc_html__('Using signatures from %s.', 'cp-integrity-checker'), esc_html($url.$version.'.json')).'</p>';
-		echo '<div class="cpic cpic-results">';
+		echo '<h2>'.esc_html__('Check core files against MD5.', 'cp-integrity-checker').'</h2>';
+		echo '<p>'.wp_kses(sprintf(__('Using signatures from %s.', 'cp-integrity-checker'), '<code>'.esc_html($url.$version.'.json').'</code>'), ['code' => [],]).'</p>';
 
+		echo '<div class="cpic cpic-results">';
 		$results = $this->check($version, $url);
 		if ($results === false) {
 			esc_html_e('Failed to get checksums.', 'cp-integrity-checker');
